742dbb2d89d75cc03c09311eaaba2e612251ac27
Adele4242 <adel.belekova@bk.ru>
2022-07-29 22:30:11 +0600

    initial

---
+++ main.js
@@ -0,0 +1,66 @@
+
+
+//FIRST
+let num = 1812
+console.log(num)
+
+//SECOND
+let a = 10
+let b = 2
+let c = 5
+console.log(a + b + c)
+
+//THIRD
+let number = 100
+console.log(number * 2)
+
+//FOURTH
+let other = 67
+console.log(other % 7)
+
+//FIFTH
+let a1 = 10
+let b1 = 2
+console.log(a1 + b1)
+console.log(a1 - b1)
+console.log(a1 * b1)
+console.log(a1 / b1)
+
+//SIXTH
+let c1 = 15
+let d = 2
+let result = c1 + d
+console.log(result)
+
+//SEVENTH
+let a2 = 30
+let b2 = 15
+let c2 = a2 + b2
+console.log(c2 % 4)
+
+//EIGHTH
+let a3 = 17
+let b3 = 10
+let c3 = a3 - b3
+let d1 = 24
+let result1 = c3 + d1
+console.log(result1)
+
+//NINTH
+let a4 = '10'
+let b4 = '99'
+let result2 = a4 + b4
+console.log(result2)
+
+//TENTH
+let name = 'лет'
+let number1 = 10
+let result3 = name + number1
+console.log(result3)
+
+
+
+
+
+
+
